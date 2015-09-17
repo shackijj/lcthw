fa42a6e85d84667bd5f15ad493bce9d20284f22f
shackijj <shaackiy@gmail.com>
2015-09-18 02:03:39 +0300

    Fix minunit.h and testing system

--- ex32/liblcthw/tests/minunit.h
+++ ex32/liblcthw/tests/minunit.h
@@ -1,4 +1,4 @@
-#undef DEBUG
+#undef NDEBUG
 #ifndef _minunit_h
 #define _minunit_h
 
@@ -9,12 +9,12 @@
 #define mu_suite_start() char *message = NULL
 
 #define mu_assert(test, message) if (!(test)) { log_err(message); return message; }
-#define mu_run_test(test) debug("\n----------%s", " ", #test);\
-    message = test(); tests_run++; if(message) return message;
+#define mu_run_test(test) debug("\n-----%s", " " #test);\
+    message = test(); tests_run++; if (message) return message;
 
 #define RUN_TESTS(name) int main(int argc, char *argv[]) {\
-    debug("---- RUNNING: %s", argv[0]);\
-        argc = argc + argc;\
+    if (argc != 1) argc = 1; \
+    debug("----- RUNNING: %s", argv[0]);\
         printf("----\nRUNNING: %s\n", argv[0]);\
         char *result = name();\
         if (result != 0) {\
