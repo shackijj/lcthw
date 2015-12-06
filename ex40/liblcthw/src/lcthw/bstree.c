8c1ba129ecbe855e7feb5da7d767332bf9c5659b
Kirill Shatsky <k.shatsky@netris.ru>
2015-12-06 00:46:10 -0500

    ex39 header and skeleton.c

---
+++ ex40/liblcthw/src/lcthw/bstree.c
@@ -0,0 +1,30 @@
+#include <lcthw/BSTree.h>
+
+BSTree *BSTree_create(BSTree_compare compare)
+{
+}
+
+void BSTree_destroy(BSTree *map)
+{
+}
+
+int BSTree_set(BSTree *map, void *key, void *data)
+{
+}
+
+void *BSTree_get(BSTree *map, void *key)
+{
+
+}
+
+
+int BSTree_traverse(BSTree *map, BSTree_traverse_cb traverse_cb)
+{
+
+}
+
+
+void *BSTree_delete(BSTree *map, void *key)
+{
+
+}
