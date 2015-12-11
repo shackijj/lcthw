309ba8afbdb37032a62a990e7228e7bb6ecc0b2b
Kirill Shatsky <k.shatsky@netris.ru>
2015-12-11 16:42:47 -0500

    ex43 done

--- ex43/liblcthw/bin/stats_util.c
+++ ex43/liblcthw/bin/stats_util.c
@@ -1,15 +1,97 @@
 #define _GNU_SOURCE
 #include <lcthw/stats.h>
 #include <lcthw/dbg.h>
+#include <lcthw/darray.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+void multicolumn(char *filename)
+{
+    size_t buff_size = 128;
+    ssize_t read;
+    float number; 
+    int rc = 0;
+    FILE *fp = NULL;
+    char *token;
+    Stats *st = NULL;
+    int line_counter = 0;
+    int column_counter = 0;
+
+    DArray *ar_stats = DArray_create(sizeof(Stats), 10);
+    check(ar_stats != NULL, "Can't create darray.");
+ 
+    fp = fopen(filename, "r");
+    check(fp != NULL, "Can't open file %s", filename);
+    
+    char *line_ptr = malloc(buff_size);
+    check_mem(line_ptr);
+
+    read = getline(&line_ptr, &buff_size, fp);
+    check(read != -1, "Can't read headers on first line.");
+    line_counter++;
+
+    token = strtok(line_ptr, " ");
+
+    while (token != NULL) {
+       if (strcmp(token, " ") != 0) {
+           st = Stats_create();
+           check(st != NULL, "Can't create stats.");
+           DArray_push(ar_stats, st);
+       }
+       token = strtok(NULL, " ");
+    }
+
+    log_info("END: %d", DArray_end(ar_stats));    
+
+    while((read = getline(&line_ptr, &buff_size, fp)) != -1 ) {
+
+        column_counter = 0;
+        line_counter++;
+
+        debug("Line is: %s", line_ptr);
+
+        token = strtok(line_ptr, " ");
+        while(token != NULL) {
+            if (strcmp(token, " ") != 0 ){
+
+                st = DArray_get(ar_stats, column_counter);
+                check(st != NULL, "Date error line %d, column %d", 
+                   line_counter, column_counter);
+
+                rc = sscanf(line_ptr, "%f", &number);
+
+                if (rc >= 0) {
+                    debug("Succesfully read: %f", number);
+                    Stats_sample(st, number);
+                }
+
+                token = strtok(NULL, " ");
+                column_counter++;
+            }
+        }
+        check(column_counter == DArray_end(ar_stats), 
+            "Date error line %d, column %d", line_counter, column_counter); 
+    }        
+
+    for (rc = 0; rc < DArray_end(ar_stats); rc++) {
+        st = ar_stats->contents[rc];
+        Stats_dump(st);
+    }
+
+//falltrough
+error:
+    if(ar_stats) DArray_clear_destroy(ar_stats);
+    if(line_ptr) free(line_ptr);
+    if(fp) fclose(fp);
+}
 
 void onecolumn(char *filename)
 {
     size_t buff_size = 128;
     ssize_t read;
     float number; 
-    int rc = 0;;
+    int rc = 0;
     FILE *fp = NULL;
 
     Stats *st = Stats_create();
@@ -37,11 +119,6 @@ error:
     if(line_ptr) free(line_ptr);
     if(st) free(st);
     if(fp) fclose(fp);
-}
-
-void multicolumn(char *filename)
-{
-    debug("[MULTI] I will open %s", filename);
 }
 
 void show_usage()
