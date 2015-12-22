6d486a00dae3ddb6a66f282786bae7f65dad41a1
shackijj <shaackiy@gmail.com>
2015-12-22 19:13:13 +0300

    descriptions of func + TODO

--- ex45/liblcthw/bin/netclient.c
+++ ex45/liblcthw/bin/netclient.c
@@ -12,13 +12,29 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+
+/*
+TODO
+
+1. Check connection closed/opened
+2. Show usage function
+3. Make it read whole web page http://c.learncodethehardway.org/book/ex45.html
+   Now it fails with 
+
+[ERROR] (src/lcthw/ringbuffer.c:82 function: RingBuffer_gets errno: None) Need more than 0 for gets, you gave: -1
+[ERROR] (bin/netclient.c:93 function: write_some errno: None) Failed to get from the buffer.
+[DEBUG] bin/netclient.c:197: failed to write to stdout.
+
+*/
 struct tagbstring NL = bsStatic("\n");
 struct tagbstring CLRF = bsStatic("\r\n");
 
 int nonblock(int fd) {
+    // Get the file access mode
     int flags = fcntl(fd, F_GETFL, 0);
     check(flags >= 0, "Invalid flags on nonblock.");
 
+    // Add NONBLOCK flag
     int rc = fcntl(fd, F_SETFL, flags | O_NONBLOCK);
     check(rc == 0, "Can't set nonblocking.");
 
@@ -30,20 +46,25 @@ error:
 int client_connect(char *host, char *port)
 {
     int rc = 0;
+    // struct describer somewhere in soket, types or netdb.
+    // we need it for creating correct socket struct.
     struct addrinfo *addr = NULL;
-
+    // DNS lookup for uri. 
     rc = getaddrinfo(host, port, NULL, &addr);
     check(rc == 0, "Failed to lookup %s:%s", host, port);
-
+    // creates  an endpoint for communication
+    // over ipv4 AF_INET, 
+    // SOCK_STREAM  Provides sequenced, reliable, two-way, connection-based
+    // byte streams
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     check(sock >= 0, "Cannot create a socket.");
-
+    // connect soket with network address
     rc = connect(sock, addr->ai_addr, addr->ai_addrlen);
     check(rc == 0, "Connect failed.");
-
+    // make socket nonblocking
     rc = nonblock(sock);
     check(rc == 0, "Can't set nonblocking.");
-
+    //!
     freeaddrinfo(addr);
     return sock;
 
@@ -61,8 +82,10 @@ int read_some(RingBuffer *buffer, int fd, int is_socket)
     }
 
     if(is_socket) {
+        // read from socket to buffer.
         rc = recv(fd, RingBuffer_starts_at(buffer), RingBuffer_available_space(buffer), 0);
     } else {
+        // read from file.
         rc = read(fd, RingBuffer_starts_at(buffer), RingBuffer_available_space(buffer));
     }
     
@@ -82,15 +105,18 @@ int write_some(RingBuffer *buffer, int fd, int is_socket, int remove_flag)
     bstring data = RingBuffer_get_all(buffer);
 
     check(data != NULL, "Failed to get from the buffer.");
-    if (remove_flag) {
+    if (remove_flag == 1) {
         check(bfindreplace(data, &NL, &CLRF, 0) == BSTR_OK, "Failed to replace NL.");
     } else {
         debug("I don't do replace");
     }
 
     if(is_socket) {
+        // Send data from buffer to connection recipient
+        // Socket should be in connected state
         rc = send(fd, bdata(data), blength(data), 0);
     } else {
+        // Write data to file.
         rc = write(fd, bdata(data), blength(data));
     }
 
@@ -105,6 +131,7 @@ error:
 
 int main(int argc, char *argv[])
 {
+    // declare file sets
     fd_set allreads;
     fd_set readmask;
 
@@ -126,40 +153,64 @@ int main(int argc, char *argv[])
                 port = argv[3];                
                 break;
             default:
+                return -1;
                 break;
        }
     }
 
     RingBuffer *in_rb = RingBuffer_create(1024 * 10);
     RingBuffer *sock_rb = RingBuffer_create(1024 * 10);
-
+    // Returns socket
     socket = client_connect(host, port);
     check(socket >= 0, "connect to %s:%s failed.", host, port);
-
+    // Clear set of descriptors
     FD_ZERO(&allreads);
+    // Add socket descriptor to allreads set
     FD_SET(socket, &allreads);
+    // Add stdin to allreads set
     FD_SET(0, &allreads);
 
     while(1) {
-        readmask = allreads;
+       // Endless loop - discrete mode. 
+       // We need this for secure handling and 
+       // gives possibility to change original set
+       readmask = allreads;
+        
+       /*
+       select() allow  a  program  to  monitor  multiple  file
+       descriptors,  waiting  until one or more of the file 
+       descriptors become "ready" for some class of 
+       I/O operation (e.g., input possible). 
+ 
+       first arg is the highest-numbered file descriptor 
+       in any of the three  sets, plus 1
+        */
+
+        // All ready for reading/wriing?
         rc = select(socket + 1, &readmask, NULL, NULL, NULL);
         check(rc >= 0, "select failed.");
+        // Ok. Here we go
 
+        // Check: Is stdin part of the set readmask?
         if(FD_ISSET(0, &readmask)) {
+            // Yes. Read from it like file. 
             rc = read_some(in_rb, 0, 0);
             check_debug(rc != -1, "failed to read from stdin.");
         }
         
+        // Check: Is socket part of the set readmask?
         if(FD_ISSET(socket, &readmask)) {
             rc = read_some(sock_rb, socket, 0);
             check_debug(rc != -1, "failed to read from socket.");
         }
 
+        // Get data from socket buffer and write it to stdout
+        // while sock_rb not empty
         while(!RingBuffer_empty(sock_rb)) {
             rc = write_some(sock_rb, 1, 0, remove_flag);
             check_debug(rc != -1, "failed to write to stdout.");
         }
-
+        // Like upper loop, but write to socket.
         while(!RingBuffer_empty(in_rb)) {
             rc = write_some(in_rb, socket, 1, remove_flag);
             check_debug(rc != -1, "Failed to write to socket.");
